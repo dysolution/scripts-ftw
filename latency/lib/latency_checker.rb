b95511090da8b4aaf088dfbd27cdff23827c183b
Jordan Peterson <dysolution@gmail.com>
2012-07-27 23:44:16 -0500

    prepare for persistence

--- latency/lib/latency_checker.rb
+++ latency/lib/latency_checker.rb
@@ -3,8 +3,26 @@ class Host
     @host_name = host_name
   end
 
-  def ping
+  def ping_time
     `ping -c 1 -n #{@host_name}` =~ /.* time=(\d+)\./
     return $1.to_i
+  end
+
+  def record_current_latency
+    LatencyMeasurement.new(@host_name, ping_time).record
+  end
+end
+
+class LatencyMeasurement
+  def initialize(host_name, latency_in_ms)
+    @host_name = host_name
+    @latency_in_ms = latency_in_ms
+  end
+
+  def inspect
+    "#{@latency_in_ms} ms to host #{@host_name}"
+  end
+
+  def record
   end
 end
--- spec/latency_checker/latency_checker_spec.rb
+++ spec/latency_checker/latency_checker_spec.rb
@@ -9,15 +9,31 @@ describe "the latency reporter" do
 end
 
 describe "the latency checker" do
-  before(:each) do
-    @hostname = 'google.com'
+  describe Host do
+    before(:each) do
+      @hostname = 'google.com'
+      @host = Host.new(@hostname)
+    end
+
+    it "should successfully ping a host" do
+      @host.ping_time.should_not be_nil
+    end
+
+    it "should return an integer ping time" do
+      @host.ping_time.should be_an(Integer)
+    end
+
+    it "should record (persist) the result" do
+      pending "need to write persistence code" do
+        @host.record_current_latency.should be_true
+      end
+    end
   end
 
-  it "should successfully ping a host" do
-    Host.new(@hostname).ping.should_not be_nil
-  end
-
-  it "ping should return an integer" do
-    Host.new(@hostname).ping.should be_an(Integer)
+  describe LatencyMeasurement do
+    it "should have a string representation" do
+      LatencyMeasurement.new('foobar', 23).inspect
+        .should == "23 ms to host foobar"
+    end
   end
 end
